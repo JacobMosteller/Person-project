88eab1e9ded1160ce05cdbac4bcce0f64e62ffbb
Jacob Mosteller <jacobmosteller722@gmail.com>
2019-06-24 20:18:40 -0600

    edits

--- server.js
+++ server.js
@@ -1,8 +1,29 @@
 const express = require('express');
-const session = require('express-session');
 const cors = require('cors');
+const session = require('express-session');
 const bodyParser = require('body-parser');
-const massive = require('masive')
-require('dotenv').config
+require('dotenv').config();
+const massive = require('massive');
 
 const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: true,
+    saveUninitialized: true,
+    cookie: {maxAge: 60000 }
+}))
+
+massive(process.env.DATABASE_URL)
+    .then((dbInstance)=>{
+        app.set('db', dbInstance)
+        console.log('Db connected')
+    })
+
+const port = process.env.PORT || 7070;
+
+app.listen(port,()=>{
+    console.log(`Running on port ${port}`)
+})  
